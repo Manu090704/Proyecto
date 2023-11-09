4dc663f227a7111b37c51f58fe34d70a5fc257a2
Manu090704 <149975766+Manu090704@users.noreply.github.com>
2023-11-08 23:21:16 -0600

    Add files via upload

--- proyectomain.cpp
+++ proyectomain.cpp
@@ -1,22 +1,22 @@
-#include"Vehiculo.h"
-#include"Arbol.h"
-#include"piedra.h"
+#include"Coche.h"
+#include"Trailer.h"
+#include"Moto.h"
 #include<iostream>
 
 int main() {
-    Vehiculo miVehiculo(4, 2018); // Crear un objeto Vehiculo con 4 llantas y año de modelo 2018
+    Coche miVehiculo(4, 2018); // Crear un objeto Vehiculo con 4 llantas y año de modelo 2018
     miVehiculo.num_de_llantas(); // Imprimir el número de llantas
     miVehiculo.anos_de_vida();   // Imprimir el año de modelo
 
     cout<<endl;
-    Arbol clasico(15,1.78);
-    clasico.anos_de_vida();
-    clasico.altura();
+    Trailer clasico(6,2011);
+    clasico.numero_de_llantas();
+    clasico.ano_de_creacion();
 
     cout<<endl;
-    Piedra cuarzo(100,0.15);
-    cuarzo.tiempo_de_vida();
-    cuarzo._altura();
+    Moto suzuki(2,2020);
+    suzuki._num_de_llantas();
+    suzuki._anos_de_vida();
 
     return 0;
 }
